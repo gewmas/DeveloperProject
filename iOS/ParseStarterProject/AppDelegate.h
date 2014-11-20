76ef30bfad3c45564862c14b38b86a956b92107b
myhgew <yuhua.mai@duke.edu>
2014-11-19 21:37:03 -0800

    Init iOS project working with Parse

---
+++ iOS/ParseStarterProject/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ParseStarterProjectAppDelegate.h
+//  ParseStarterProject
+//
+//  Copyright 2014 Parse, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class ParseStarterProjectViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, strong) IBOutlet UIWindow *window;
+
+@end
---
+++ iOS/ParseStarterProject/MainViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.redColor();
+        
+        // Test parse
+        var gameScore = PFObject(className: "GameScore")
+        gameScore.setObject(1337, forKey: "score")
+        gameScore.setObject("Sean Plott", forKey: "playerName")
+        gameScore.saveInBackgroundWithBlock {
+            (success: Bool!, error: NSError!) -> Void in
+            if (success != nil) {
+                NSLog("Object created with id: \(gameScore.objectId)")
+            } else {
+                NSLog("%@", error)
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
